3a42efcc2f8e2e6756a4c1b36105fca44d1aa01c
Jen <linjennifer106@gmail.com>
2020-05-07 21:10:41 -0400

    try if else

--- setup.sql
+++ setup.sql
@@ -207,10 +207,25 @@ delimiter //
 DROP PROCEDURE IF EXISTS CovidSortBy //
 CREATE PROCEDURE CovidSortBy(IN covid_date VARCHAR(40), covid_attribute VARCHAR(40))
 BEGIN
-  SET @sql = CONCAT('SELECT Country.name, DailyCOVID19Reports.numConfirmed, DailyCOVID19Reports.numDeaths, DailyCOVID19Reports.numRecovered FROM DailyCOVID19Reports, Country WHERE DailyCOVID19Reports.countryId = Country.countryId AND date = ', covid_date, ' ORDER BY ', covid_attribute,' DESC');
-  PREPARE stmt FROM @sql;
-  EXECUTE stmt;
-  DEALLOCATE PREPARE stmt;
+  IF covid_attribute = 'numConfirmed' THEN
+    SELECT Country.name, DailyCOVID19Reports.numConfirmed, DailyCOVID19Reports.numDeaths, DailyCOVID19Reports.numRecovered
+    FROM DailyCOVID19Reports, Country
+    WHERE DailyCOVID19Reports.countryId = Country.countryId, date = covid_date
+    WHERE DailyCOVID19Reports.countryId = Country.countryId AND date = covid_date
+    ORDER BY numConfirmed;
+  ELSEIF covid_attribute = 'numDeaths' THEN
+    SELECT Country.name, DailyCOVID19Reports.numConfirmed, DailyCOVID19Reports.numDeaths, DailyCOVID19Reports.numRecovered
+    FROM DailyCOVID19Reports, Country
+    WHERE DailyCOVID19Reports.countryId = Country.countryId, date = covid_date
+    WHERE DailyCOVID19Reports.countryId = Country.countryId AND date = covid_date
+    ORDER BY numDeaths;
+  ELSE
+    SELECT Country.name, DailyCOVID19Reports.numConfirmed, DailyCOVID19Reports.numDeaths, DailyCOVID19Reports.numRecovered
+    FROM DailyCOVID19Reports, Country
+    WHERE DailyCOVID19Reports.countryId = Country.countryId, date = covid_date
+    WHERE DailyCOVID19Reports.countryId = Country.countryId AND date = covid_date
+    ORDER BY numRecovered;
+  END IF;
 END;
 //
 delimiter ;
