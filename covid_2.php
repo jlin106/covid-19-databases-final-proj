e6663bec1c9ceece76e3fc626143cbae3613c7fa
Jen <linjennifer106@gmail.com>
2020-05-09 13:34:28 -0400

    added procedure

--- covid_2.php
+++ covid_2.php
@@ -38,39 +38,36 @@ echo "<h3> Countries of Interest: ";
 echo $countries_list;
 echo "</h3>";
 
-// It returns true if first statement executed successfully; false otherwise.
-// Results of first statement are retrieved via $mysqli->store_result()
-// from which we can call ->fetch_row() to see successive rows
-if ($mysqli->multi_query("CALL CovidTimeSeries('".$country."');")) {
+echo "<table border=\"1px solid black\">";
+outputResultsTableHeader();
 
-   // Check if a result was returned after the call
-   if ($result = $mysqli->store_result()) {
-
-       echo "<table border=\"1px solid black\">";
-       $row = $result->fetch_row();
-
-       // Output appropriate table header row
-       outputResultsTableHeader();
-
-       // Output each row of resulting relation
-       do {
-           echo "<tr>";
-           for($i = 0; $i < sizeof($row); $i++){
-               echo "<td>" . $row[$i] . "</td>";
-           }
-           echo "</tr>";
-       } while($row = $result->fetch_row());
-       echo "</table>";
-       $result->close();
-
-   }
-
-// The "multi_query" call did not end successfully, so report the error
-// This might indicate we've called a stored procedure that does not exist,
-// or that database connection is broken
-} else {
-       printf("<br>Error: %s\n", $mysqli->error);
+// Retrieving each selected option
+foreach ($countries as $country) {
+  // It returns true if first statement executed successfully; false otherwise.
+  // Results of first statement are retrieved via $mysqli->store_result()
+  // from which we can call ->fetch_row() to see successive rows
+  if ($mysqli->multi_query("CALL CovidByCountry('".$covid_date."','".$country."');")) {
+     // Check if a result was returned after the call
+     if ($result = $mysqli->store_result()) {
+         $row = $result->fetch_row();
+         // Output each row of resulting relation
+         do {
+             echo "<tr>";
+             for($i = 0; $i < sizeof($row); $i++){
+                 echo "<td>" . $row[$i] . "</td>";
+             }
+             echo "</tr>";
+         } while($row = $result->fetch_row());
+         $result->close();
+     }
+  // The "multi_query" call did not end successfully, so report the error
+  // This might indicate we've called a stored procedure that does not exist,
+  // or that database connection is broken
+  } else {
+         printf("<br>Error: %s\n", $mysqli->error);
+  }
 }
+echo "</table>";
 
 // Close the connection created above by including 'open.php' at top of this file
 mysqli_close($mysqli);
