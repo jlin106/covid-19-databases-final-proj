704d100a2770562229ef8cb194e24a3ca99b1bf5
Jen <linjennifer106@gmail.com>
2020-05-11 18:15:12 -0400

    fluff population php

--- population.php
+++ population.php
@@ -6,8 +6,18 @@
 ?>
 <body>
     <h2> Population and Covid-19 Data</h2>
+    <p> Population statistics: </p>
+    <ul>
+      <li>Estimated Population Size</li>
+      <li>Population Density</li>
+      <li>Population Rate of Increase</li>
+      <li>Life Expectancy</li>
+      <li>Infant Mortality Rate</li>
+      <li>Fertility Rate</li>
+    </ul>
     <div class="container row">
-         <h3> Option 1: Population data from specified countries </h3>
+         <h3> Option 1: Query by country </h3>
+         <p> View Population statistics along with Covid-19 statistics of selected countries </p>
          <div class="column left">
            <form action="population_1.php" method="post">
             <label for="country[]">Select country/countries of interest:</label>
@@ -28,13 +38,19 @@
        </div>
        <br>
        <div class="container row">
-         <h3> Option 2: Population data sorted by COVID data</h3>
+         <h3> Option 2: Query sort based on Covid-19 statistic </h3>
+         <p> Sort the data based on a Covid-19 statistic and view the top few or bottom few countries' data. </p>
+         <p> (Ex. View 5 countries' population data with the highest number of confirmed cases) </p>
          <div class="column left">
            <form action="population_2.php" method="post">
-             View <select id="topbottom" name="topbottom">
+             View either the top countries or bottom countries
+             <select id="topbottom" name="topbottom">
                <option value="top">top</option>
                <option value="bottom">bottom</option>
-             </select> (1-235) <input type="text" name="number"> countries by
+             </select>
+             Enter number of countries to display (1-235)
+             <input type="text" name="number">
+             Sort by Covid-19 statistic:
              <select id="attribute" name="attribute">
                <option value="numConfirmed">Number of Confirmed Cases</option>
                <option value="numDeaths">Number of Deaths</option>
@@ -47,13 +63,19 @@
        </div>
        <br>
        <div class="container row">
-         <h3> Option 3: Data sorted by population data </h3>
+         <h3> Option 3: Query sort based on Population statistic </h3>
+         <p> Sort the data based on an Population statistic and view the top few or bottom few countries' data. </p>
+         <p> (Ex. View 5 countries' population data with the highest Estimated Population Size) </p>
          <div class="column left">
            <form action="population_3.php" method="post">
-             View <select id="topbottom" name="topbottom">
+             View either the top countries or bottom countries
+             <select id="topbottom" name="topbottom">
                <option value="top">top</option>
                <option value="bottom">bottom</option>
-             </select> (1-235) <input type="text" name="number"> countries by
+             </select>
+             Enter number of countries to display (1-235)
+             <input type="text" name="number">
+             Sort by Covid-19 statistic:
              <select id="attribute" name="attribute">
                <option value="estPopSize">Estimated Population Size</option>
                <option value="popDensity">Population Density</option>
