b8d89176e05e99325dc82dfd984588fd70351b50
Jen <linjennifer106@gmail.com>
2020-05-11 18:10:21 -0400

    added fluff to trade

--- trade.php
+++ trade.php
@@ -6,8 +6,23 @@
     include 'open.php';
   ?>
   <h2> Trade and Covid-19 Data </h2>
+  <p> Trade statistics: </p>
+  <ul>
+    <li>Major Trading Partners - Imports</li>
+    <ul>
+      <li>Country Name</li>
+      <li>Percent Import of Total Trade</li>
+    </ul>
+    <li>Major Trading Partners - Exports</li>
+    <ul>
+      <li>Country Name</li>
+      <li>Percent Export of Total Trade</li>
+    </ul>
+  </ul>
   <div class="container row">
-    <h3> Option 1: Trade data from specified countries </h3>
+    <h3> Option 1: Query by country </h3>
+    <p> For the selected countries, display their major trading partners (imports and exports)
+      alone with some trade statistics and Covid-19 statistics </p>
     <div class="column left">
       <form action="trade_1.php" method="post">
         <label for="country[]">Select country/countries of interest:</label>
@@ -23,52 +38,6 @@
          </select>
          <br>
          <input type="submit" value="Submit">
-      </form>
-    </div>
-  </div>
-  <br>
-  <div class="container row">
-    <h3> Option 2: Query by date and country </h3>
-    <div class="column left">
-      <form action="covid_2.php" method="post">
-        <label for="covid_date">Enter date (01/22/2020 to 05/08/2020):</label>
-        <br>
-        <input type="date" id="covid_date" name="covid_date"
-               min="2020-01-22" max="2020-05-08">
-        <br>
-        <br>
-        <label for="country[]">Select country/countries of interest:</label>
-	      <br>
-	      <select id="country[]" name="country[]" multiple>
-          <?php
-            $countries = $mysqli->query("SELECT name FROM Country");
-            while ($rows = $countries->fetch_assoc()) {
-              $country_name = $rows['name'];
-              echo "<option value='$country_name'>$country_name</option>";
-            }
-          ?>
-        </select>
-        <input type="submit" value="Submit">
-      </form>
-    </div>
-  </div>
-  <br>
-  <div class="container row">
-    <h3> Option 3: Time series by country</h3>
-    <div class="column left">
-      <form action="covid_3.php" method="post">
-        <label for="country">Select one country of interest:</label>
-	      <br>
-	      <select id="country" name="country">
-          <?php
-          $countries = $mysqli->query("SELECT name FROM Country");
-            while ($rows = $countries->fetch_assoc()) {
-              $country_name = $rows['name'];
-              echo "<option value='$country_name'>$country_name</option>";
-            }
-          ?>
-        </select>
-        <input type="submit" value="Submit">
       </form>
     </div>
   </div>
