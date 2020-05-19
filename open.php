ba1a14e31ff86027a2902ead55470323253ca1fb
Jennifer Lin <jlin123@jhu.edu>
2020-05-18 23:28:21 -0400

    deleted duplicates

--- open.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include 'dbase-conf.php';
-    $mysqli = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
-
-    if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        die ('Error connecting to mysql. :-( <br/>');
-    }
-?>
